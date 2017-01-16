42d082024eccba8adf000e0bec0ab82450712af3
Sander Mathijs van Veen <sander@leaningtech.com>
2017-01-16 09:45:36 +0000

    Add script for converting emscripten benchmark results to CSV

---
+++ tests/parse_emscripten_benchmark.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+import sys
+import re
+
+grid = defaultdict(list)
+
+bench_name = ''
+bench_names = []
+engine_names = []
+
+found_all_engines = False
+
+for line in file(sys.argv[1]):
+    if line.startswith('test_'):
+        bench_name = line[5:].split(' ')[0]
+        bench_names.append(bench_name)
+        continue
+
+    match = re.match('^\s*([\w-]+): mean: (\d+.\d+)', line)
+    if match:
+        if match.group(1) not in engine_names:
+            engine_names.append(match.group(1))
+            assert not found_all_engines
+        else:
+            found_all_engines = True
+
+        grid[bench_name].append(match.group(2))
+
+print('engine,' + ','.join(bench_names))
+
+for e, engine_name in enumerate(engine_names):
+    row = engine_name
+    for bench_name in bench_names:
+        row += ',' + grid[bench_name][e]
+    print(row)
